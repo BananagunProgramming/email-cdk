15c311e749283153dbf88e41ea551bacb83ed5af
David Nash <bananagun@live.com>
2022-11-16 20:15:33 -0500

    formatting

--- bin/email-cdk.ts
+++ bin/email-cdk.ts
@@ -1,10 +1,8 @@
 #!/usr/bin/env node
-import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
 import { EmailCdkStack } from '../lib/email-cdk-stack';
 
 const app = new cdk.App();
-
 new EmailCdkStack(app, 'EmailCdkStack', {
   env: {
     account: '460032395895',
--- lib/email-cdk-stack.ts
+++ lib/email-cdk-stack.ts
@@ -1,6 +1,6 @@
 import * as cdk from 'aws-cdk-lib';
-import { CodePipeline, CodePipelineSource, ShellStep, ManualApprovalStep } from 'aws-cdk-lib/pipelines';
 import { Construct } from 'constructs';
+import { CodePipeline, CodePipelineSource, ShellStep, ManualApprovalStep } from 'aws-cdk-lib/pipelines';
 import { EmailPipelineStage } from './email-pipeline-stage';
 
 export class EmailCdkStack extends cdk.Stack {
@@ -10,7 +10,7 @@ export class EmailCdkStack extends cdk.Stack {
     const pipeline = new CodePipeline(this, 'EmailCdkPipeline', {
       pipelineName: 'EmailPipeline',
       synth: new ShellStep('Synth', {
-        input: CodePipelineSource.gitHub('bananagun/email-cdk','main'),
+        input: CodePipelineSource.gitHub('BananagunProgramming/email-cdk','main'),
         commands: ['npm ci', 'npm run build', 'npx cdk synth']
       })
     });
