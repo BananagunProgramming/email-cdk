c3e19d2a990c5ac5db6e104c6dfe41ad5fdc6c39
David Nash <bananagun@live.com>
2022-11-17 08:58:24 -0500

    add ticks back in?

--- lib/apigateway/api-gateway-stack.ts
+++ lib/apigateway/api-gateway-stack.ts
@@ -29,7 +29,7 @@ export class ApiGatewayStack extends cdk.Stack {
             options: {
                 credentialsRole: integrationRole,
                 requestParameters: {
-                    'integration.request.header.Content-Type': 'application/json',
+                    'integration.request.header.Content-Type': `'application/json'`,
                 },
                 requestTemplates: {
                     'application/json': 'Action=SendMessage&MessageBody=$input.body',
